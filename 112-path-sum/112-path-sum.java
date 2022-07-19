4236cb6acbcdcbd7738bf4341993e82090200441
Sarwesh Khairnar <62764698+Sarwesh2003@users.noreply.github.com>
2022-07-19 12:18:19 +0530

    Time: 5 ms (5.57%), Space: 44.4 MB (6.47%) - LeetHub

--- 112-path-sum/112-path-sum.java
+++ 112-path-sum/112-path-sum.java
@@ -19,19 +19,43 @@ class Solution {
             return false;
         }
         
-        return Sum(root, targetSum, 0);
-     
-    }
-    public boolean Sum(TreeNode root, int tgt, int sum){
-        if(root == null) return false;
-        
-        sum += root.val;
-        if(tgt == sum && root.left == null && root.right == null)   return true;
-        
-        if(Sum(root.left, tgt, sum) || Sum(root.right, tgt, sum)){
-            return true;
+        Stack<Integer> sum = new Stack<>();
+        Stack<TreeNode> s = new Stack<>();
+        s.push(root);
+        sum.push(root.val);
+        while(!s.empty()){
+            TreeNode node = s.pop();
+            int currSum = sum.pop();
+            if(currSum == targetSum && node.right == null && node.left == null){
+                return true;
+            }
+            
+            if(node.right != null){
+                s.push(node.right);
+                sum.push(currSum + node.right.val);
+            }
+            
+            if(node.left != null){
+                s.push(node.left);
+                sum.push(currSum + node.left.val);
+            }
         }
         return false;
-        
+        // return Sum(root, targetSum, 0); 
     }
+    
+    
+//     O(2^n)
+//     public boolean Sum(TreeNode root, int tgt, int sum){
+//         if(root == null) return false;
+        
+//         sum += root.val;
+//         if(tgt == sum && root.left == null && root.right == null)   return true;
+        
+//         if(Sum(root.left, tgt, sum) || Sum(root.right, tgt, sum)){
+//             return true;
+//         }
+//         return false;
+        
+//     }
 }
