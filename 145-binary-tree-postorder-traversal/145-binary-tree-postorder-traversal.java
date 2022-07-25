4e1916dbad2d4ede77bc89e010daee9cad22d23e
Sarwesh Khairnar <62764698+Sarwesh2003@users.noreply.github.com>
2022-07-25 13:40:26 +0530

    Time: 1 ms (51.06%), Space: 41.7 MB (77.63%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
@@ -16,9 +16,48 @@
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
-        solve(root, list);
+        // solve(root, list);
+        morrisTraversal(root, list);
         return list;
     }
+    
+    public void morrisTraversal(TreeNode root, List<Integer> list){
+        TreeNode curr = root;
+        while(curr != null){
+            if(curr.right == null){
+                list.add(curr.val);
+                curr = curr.left;
+            }else{
+                TreeNode pred = curr.right;
+                
+                while(pred.left != null && pred.left != curr){
+                    pred = pred.left;
+                }
+                if(pred.left == null){
+                    list.add(curr.val);
+                    pred.left = curr;
+                    curr = curr.right;
+                }else{
+                    pred.left = null;
+                    // list.add(curr.val);
+                    curr = curr.left;
+                }
+            }
+        }
+        Collections.reverse(list);
+    }
+    
+    public TreeNode predecesor(TreeNode root, TreeNode curr){
+        TreeNode temp = root;
+        while(temp.right != null && temp.right != curr){
+            temp = temp.right;
+        }
+        return temp;
+    }
+    
+    
+    
+    // TC: O(N) SC: O(N)
     public void solve(TreeNode root, List<Integer> list){
         if(root == null){
             return;
