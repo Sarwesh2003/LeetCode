d17a9f49193f1c5c3c11b39f026b129a41e9d579
Sarwesh Khairnar <62764698+Sarwesh2003@users.noreply.github.com>
2022-07-27 10:16:31 +0530

    Time: 1 ms (78.58%), Space: 43 MB (28.80%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
@@ -15,21 +15,32 @@
  */
 class Solution {
     public void flatten(TreeNode root) {
-        ArrayList<Integer> list = new ArrayList<>();
-        preorder(root, list);
+//         ArrayList<Integer> list = new ArrayList<>();
+//         preorder(root, list);
         
-        // root.left = null;
-        // root.right = null;
-        // root = null;
-        // root = new TreeNode(list.get(0));
-        TreeNode r = root;
-        for(int i=1; i<list.size(); ++i){
-            r.right = new TreeNode(list.get(i));
-            r.left = null;
-            r = r.right;
+//         TreeNode r = root;
+//         for(int i=1; i<list.size(); ++i){
+//             r.right = new TreeNode(list.get(i));
+//             r.left = null;
+//             r = r.right;
+//         }
+        
+        TreeNode curr = root;
+        while(curr != null){
+            if(curr.left != null){
+                TreeNode p = curr.left;
+                while(p.right != null){
+                    p = p.right;
+                }
+                p.right = curr.right;
+                curr.right = curr.left;
+            }
+            curr.left = null;
+            curr = curr.right;
         }
     }
     
+//  O(N) aopproach   
     public void preorder(TreeNode root, ArrayList<Integer> list){
         if(root == null){
             return;
