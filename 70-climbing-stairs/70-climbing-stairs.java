21de423c034a6520a0dbcc0beabebb9e34055b20
Sarwesh Khairnar <62764698+Sarwesh2003@users.noreply.github.com>
2022-10-08 19:26:02 +0530

    Time: 0 ms (100.00%), Space: 41 MB (31.00%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,9 +1,10 @@
 class Solution {
     public int climbStairs(int n) {
         int[] dp = new int[n + 1];
-        return solve(n, dp);
+        return solve(n);
     }
     
+    // Memoization
     public int solve(int n, int[] dp){
         if(n == 0 || n == 1){
             return 1;
@@ -13,5 +14,17 @@ class Solution {
         }
         dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
         return dp[n];
+    }
+    
+    // Space Optimization
+    public int solve(int n){
+        int prev1 = 1;
+        int prev2 = 0;
+        for(int i = 0; i < n; ++i){
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
     }
 }
