d8c792f76d262f50e113680652c77f2f79a25e2f
Sarwesh Khairnar <62764698+Sarwesh2003@users.noreply.github.com>
2022-10-08 20:00:56 +0530

    Time: 1 ms (92.49%), Space: 41.8 MB (52.12%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.java
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.java
@@ -1,19 +1,26 @@
 class Solution {
     public List<Integer> getRow(int rowIndex) {
-        List<List<Integer>> list = new ArrayList<>();
-        // 1st row
-        list.add(new ArrayList<>(Arrays.asList(1)));
+//         List<List<Integer>> list = new ArrayList<>();
+//         // 1st row
+//         list.add(new ArrayList<>(Arrays.asList(1)));
         
-        for(int i = 1; i <= rowIndex; ++i){
-            List<Integer> lastRow = list.get(i - 1);
-            List<Integer> curr = new ArrayList<>();
-            curr.add(1);
-            for(int j = 1; j < (i); ++j){
-                curr.add(lastRow.get(j - 1) + lastRow.get(j));
-            }
-            curr.add(1);
-            list.add(curr);
+//         for(int i = 1; i <= rowIndex; ++i){
+//             List<Integer> lastRow = list.get(i - 1);
+//             List<Integer> curr = new ArrayList<>();
+//             curr.add(1);
+//             for(int j = 1; j < (i); ++j){
+//                 curr.add(lastRow.get(j - 1) + lastRow.get(j));
+//             }
+//             curr.add(1);
+//             list.add(curr);
+//         }
+//         return list.get(rowIndex);
+        List<Integer> list = new ArrayList<>();
+        double ncr = 1.0;
+        for(int i = 0; i <= rowIndex; ++i){
+            list.add((int)ncr);
+            ncr = (ncr * (rowIndex - i)) / (i + 1);
         }
-        return list.get(rowIndex);
+        return list;
     }
 }
