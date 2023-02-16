7c27f682134311526697006c754205747362d2e1
Sarwesh Khairnar <62764698+Sarwesh2003@users.noreply.github.com>
2023-02-16 18:53:54 +0530

    Added solution - LeetHub

---
+++ Subset II - GFG/subset-ii.java
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        PrintWriter ot = new PrintWriter(System.out);
+        int t;
+        t = sc.nextInt();
+        while (t-- > 0) {
+            int n;
+            n = sc.nextInt();
+            int[] nums = new int[n];
+            for (int i = 0; i < n; i++) nums[i] = sc.nextInt();
+
+            Solution obj = new Solution();
+            ArrayList<ArrayList<Integer>> ans = obj.printUniqueSubsets(nums);
+            Collections.sort(ans, new Comparator<ArrayList<Integer>>() {
+                @Override   public int compare(ArrayList<Integer> a,
+                                             ArrayList<Integer> b) {
+                    int al = a.size();
+                    int bl = b.size();
+                    int min = Math.min(al, bl);
+                    for (int i = 0; i < min; i++) {
+                        if (a.get(i) < b.get(i))
+                            return -1;
+                        else if (a.get(i) > b.get(i))
+                            return 1;
+                    }
+                    if (al < bl)
+                        return -1;
+                    else if (al > bl)
+                        return 1;
+                    return -1;
+                }
+            });
+            ot.print("[ ");
+            for (int i = 0; i < ans.size(); i++) {
+                ot.print("[ ");
+                for (int j = 0; j < ans.get(i).size(); j++)
+                    ot.print(ans.get(i).get(j) + " ");
+                ot.print("]");
+            }
+            ot.println(" ]");
+        }
+        ot.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public static ArrayList<ArrayList<Integer>> printUniqueSubsets(int[] nums) {
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        ans.add(new ArrayList<>());
+        solve(nums, ans, 0, new ArrayList<>());
+        return ans;
+    }
+    public static void solve(int[] nums, ArrayList<ArrayList<Integer>> ans, int idx, List<Integer> list){
+        if(idx >= nums.length){
+            return;
+        }
+        for(int i = idx; i < nums.length; ++i){
+            if(i > idx && nums[i] == nums[i - 1])continue;
+            list.add(nums[i]);
+            ans.add(new ArrayList<>(list));
+            solve(nums, ans, i + 1, list);
+            list.remove(list.size() - 1);
+        }
+        
+    }
+}
